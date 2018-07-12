33a2ed99509ea75653d1220efa1132bc5207fc82
SakiKato2 <saki.kato2017@gmail.com>
2018-07-12 14:47:17 +0900

    Update controller code

--- src/SakiController.cpp
+++ src/SakiController.cpp
@@ -1,11 +1,12 @@
 #include "SakiController.h"
 
+
 SakiController::SakiController(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config)
 : mc_control::MCController(rm, dt)
 {
   config_.load(config);
   solver().addConstraintSet(contactConstraint);
-  solver().addConstraintSet(kinematicsConstraint);
+  solver().addConstraintSet(dynamicsConstraint);
   solver().addTask(postureTask);
   solver().setContacts({{}});
 
@@ -20,6 +21,39 @@ bool SakiController::run()
 void SakiController::reset(const mc_control::ControllerResetData & reset_data)
 {
   mc_control::MCController::reset(reset_data);
-}
+  solver().setContacts({
+                      {robots(), "LFullSole", "AllGround"},
+                      {robots(), "RFullSole", "AllGround"}
+  });
+  com_ = std::make_shared<mc_tasks::CoMTask>(robots(), 0);
+  solver().addTask(com_);
+  ef_ = std::make_shared<mc_tasks::EndEffectorTask>("RARM_LINK6", robots(), 0);
+  solver().addTask(ef_);
+  ef_->selectActiveJoints(solver(), {
+                                     "RARM_JOINT0",
+                                     "RARM_JOINT1",
+                                     "RARM_JOINT2",
+                                     "RARM_JOINT3",
+                                     "RARM_JOINT4",
+                                     "RARM_JOINT5",
+                                     "RARM_JOINT6"}); 
 
+ ef_->set_ef_pose({
+                    Eigen::Quaterniond(0.56, -0.46, 0.54, -0.43),
+                    Eigen::Vector3d(0.15, -0.23, 0.73)});
+
+ /*ef_->set_ef_pose({
+                    Eigen::Quaterniond(0.62, -0.32, 0.70, -0.16),
+                    Eigen::Vector3d(0.65, -0.15, 0.76)});*/
+}
 CONTROLLER_CONSTRUCTOR("SakiController", SakiController)
+
+
+	/*printf("まっています\n");
+	sleep(30);
+	printf("待ちました\n");
+	return 0;*/
+ /* ef_->set_ef_pose({
+                    Eigen::Quaterniond(0.44, -0.53, 0.58, -0.42),
+                    Eigen::Vector3d(0.70, -0.25, 0.74)});*/
+
--- src/SakiController.h
+++ src/SakiController.h
@@ -2,6 +2,9 @@
 
 #include <mc_control/mc_controller.h>
 
+#include <mc_tasks/CoMTask.h>
+#include <mc_tasks/EndEffectorTask.h>
+
 
 struct MC_CONTROL_DLLAPI SakiController : public mc_control::MCController
 {
@@ -12,4 +15,6 @@ struct MC_CONTROL_DLLAPI SakiController : public mc_control::MCController
     void reset(const mc_control::ControllerResetData & reset_data) override;
 private:
     mc_rtc::Configuration config_;
+    std::shared_ptr<mc_tasks::CoMTask> com_;
+    std::shared_ptr<mc_tasks::EndEffectorTask> ef_;
 };
