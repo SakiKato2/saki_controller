e648352efd2780f1d7547bb2d41ff5774c8ee25a
SakiKato2 <41132380+SakiKato2@users.noreply.github.com>
2018-08-22 17:44:46 +0900

    Add files via upload

--- src/SakiController.cpp
+++ src/SakiController.cpp
@@ -1,5 +1,5 @@
 #include "SakiController.h"
-
+#include <iostream>
 
 SakiController::SakiController(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config)
 : mc_control::MCController(rm, dt)
@@ -15,45 +15,78 @@ SakiController::SakiController(mc_rbdyn::RobotModulePtr rm, double dt, const mc_
 
 bool SakiController::run()
 {
-  return mc_control::MCController::run();
+  //std::cout << "waiting for the preparation" << std::endl;
+  bool ret =  mc_control::MCController::run();
+  std::cout << timeStep << std::endl;
+  if(running == false) {
+    return ret;
+  }
+  if(running_count%1500==0){
+    if(up){
+      ef_->set_ef_pose({
+          Eigen::Quaterniond(0.56, -0.46, 0.55, -0.44),
+            Eigen::Vector3d(0.21, -0.24, 0.97)});
+    }
+    else{
+      ef_->set_ef_pose({
+          Eigen::Quaterniond(0.77, 0, 0, -0.04),
+            Eigen::Vector3d(0.24, -0.25, 0.97)});
+    }
+    up = !up;
+  }
+  running_count++;
+  if(running_count > 3000) {
+    running = false;
+    running_count = 0;
+    up = true;
+  }
+  
+  return ret;
 }
+
 
 void SakiController::reset(const mc_control::ControllerResetData & reset_data)
 {
+
   mc_control::MCController::reset(reset_data);
   solver().setContacts({
-                      {robots(), "LFullSole", "AllGround"},
-                      {robots(), "RFullSole", "AllGround"}
-  });
+      {robots(), "LFullSole", "AllGround"},
+        {robots(), "RFullSole", "AllGround"}
+    });
   com_ = std::make_shared<mc_tasks::CoMTask>(robots(), 0);
   solver().addTask(com_);
   ef_ = std::make_shared<mc_tasks::EndEffectorTask>("RARM_LINK6", robots(), 0);
   solver().addTask(ef_);
   ef_->selectActiveJoints(solver(), {
-                                     "RARM_JOINT0",
-                                     "RARM_JOINT1",
-                                     "RARM_JOINT2",
-                                     "RARM_JOINT3",
-                                     "RARM_JOINT4",
-                                     "RARM_JOINT5",
-                                     "RARM_JOINT6"}); 
+      "RARM_JOINT0",
+        "RARM_JOINT1",
+        "RARM_JOINT2",
+        "RARM_JOINT3",
+        "RARM_JOINT4",
+        "RARM_JOINT5",
+        "RARM_JOINT6"}); 
+  std::cout << "set_ef_pose start" << std::endl;
+  ef_->set_ef_pose({
+      Eigen::Quaterniond(0.56, -0.46, 0.55, -0.44),
+            Eigen::Vector3d(0.21, -0.24, 0.97)});
+  std::cout << "set_ef_pose end" << std::endl;
 
- ef_->set_ef_pose({
-                    Eigen::Quaterniond(0.56, -0.46, 0.54, -0.43),
-                    Eigen::Vector3d(0.15, -0.23, 0.73)});
-
- /*ef_->set_ef_pose({
-                    Eigen::Quaterniond(0.62, -0.32, 0.70, -0.16),
-                    Eigen::Vector3d(0.65, -0.15, 0.76)});*/
+  gui_->addElement({"SakiController"},
+                   mc_rtc::gui::Button("Run",
+                                       [this]() {this->running = true;}));
+  running = false;
+  running_count = 0;
+  up = true;
 }
 CONTROLLER_CONSTRUCTOR("SakiController", SakiController)
 
-
-	/*printf("まっています\n");
-	sleep(30);
-	printf("待ちました\n");
-	return 0;*/
- /* ef_->set_ef_pose({
-                    Eigen::Quaterniond(0.44, -0.53, 0.58, -0.42),
-                    Eigen::Vector3d(0.70, -0.25, 0.74)});*/
-
+  ////////////////////
+  // From Test code //
+  ////////////////////
+  // MCController::reset(reset_data);
+  // /* Reset the task to the current end-effector position */
+  // efTask->reset();
+  // comTask->reset();
+  // /* Move the end-effector 10cm forward, 10 cm to the right and 10 cm upward */
+  // efTask->set_ef_pose(sva::PTransformd(sva::RotY<double>(-M_PI/2),
+  //                                      efTask->get_ef_pose().translation() + Eigen::Vector3d(0.3, -0.1, 0.2)));
--- src/SakiController.h
+++ src/SakiController.h
@@ -2,8 +2,19 @@
 
 #include <mc_control/mc_controller.h>
 
+
+#include <mc_control/mc_controller.h>
+#include <mc_tasks/OrientationTask.h>
+#include <mc_tasks/ComplianceTask.h>
+#include <mc_tasks/TrajectoryTask.h>
 #include <mc_tasks/CoMTask.h>
 #include <mc_tasks/EndEffectorTask.h>
+
+#include <Tasks/QPContactConstr.h>
+#include <mc_rtc/logging.h>
+#include <RBDyn/EulerIntegration.h>
+#include <RBDyn/FK.h>
+#include <RBDyn/FV.h>
 
 
 struct MC_CONTROL_DLLAPI SakiController : public mc_control::MCController
@@ -17,4 +28,13 @@ private:
     mc_rtc::Configuration config_;
     std::shared_ptr<mc_tasks::CoMTask> com_;
     std::shared_ptr<mc_tasks::EndEffectorTask> ef_;
+    std::shared_ptr<mc_tasks::OrientationTask> ori_;
+    //std::shared_ptr<mj_traj::mj_trajectory> trajectoryTask = nullptr;
+    private:
+    bool ready = true;
+    bool first_traj = true;
+
+    bool running;
+    bool up;
+    int running_count;
 };
